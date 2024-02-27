1d65a1f6253b883996c6565f85643c914996f47a
kevinloeffler <hi@kevinloeffler.ch>
2024-02-27 16:02:10 +0100

    remove unused code

--- src/lib/components/UploadImage.svelte
+++ src/lib/components/UploadImage.svelte
@@ -1,20 +1,7 @@
-<button on:click={handleHelloClick}>Hello from Rust</button>
-<button on:click={loadImage}>Load image</button>
-
 <button on:click={uploadImage}>Upload image</button>
 
 <script lang="ts">
     import { invoke } from '@tauri-apps/api/tauri'
-
-    // const invoke = window.__TAURI__.invoke
-    function handleHelloClick() {
-        invoke('my_custom_command', {msg: 'Joel'})
-    }
-
-    async function loadImage() {
-        const result = await invoke('load_image', {path: '/Users/kl/Kevin/Projects/ASLA/temp images/cre_6087_11.jpg'})
-        console.log('result:', result)
-    }
 
     async function uploadImage() {
         try {

fb17d3d9e44d51055efd41a97479a6d6965d2ea0
kevinloeffler <hi@kevinloeffler.ch>
2024-06-05 15:37:19 +0200

    prevent app crash when windows are not found

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -1,17 +1,42 @@
-use tauri::{Manager, Window};
+use tauri::{AppHandle, Manager};
 
 #[tauri::command]
-pub fn start_app(window: Window) {
-    window.get_window("launch").expect("launch window not found").close().unwrap();
-    window.get_window("main").expect("main window not found").show().unwrap();
+pub fn start_app(app_handle: AppHandle) {
+    if let Some(launch_window) = app_handle.get_window("launch") {
+        if let Err(e) = launch_window.close() {
+            println!("Failed to close launch window: {}", e);
+        }
+    } else {
+        println!("Launch window not found");
+    }
+
+    if let Some(main_window) = app_handle.get_window("main") {
+        if let Err(e) = main_window.show() {
+            println!("Failed to show main window: {}", e);
+        }
+    } else {
+        println!("Main window not found");
+    }
 }
 
 #[tauri::command]
-pub fn show_welcome_window(window: Window) {
-    window.get_window("welcome").expect("welcome window not found").show().unwrap();
+pub fn show_welcome_window(app_handle: AppHandle) {
+    if let Some(welcome_window) = app_handle.get_window("welcome") {
+        if let Err(e) = welcome_window.show() {
+            println!("Failed to show welcome window: {}", e);
+        }
+    } else {
+        println!("Welcome window not found");
+    }
 }
 
 #[tauri::command]
-pub fn hide_welcome_window(window: Window) {
-    window.get_window("welcome").expect("welcome window not fount").show().unwrap();
+pub fn hide_welcome_window(app_handle: AppHandle) {
+    if let Some(welcome_window) = app_handle.get_window("welcome") {
+        if let Err(e) = welcome_window.hide() {
+            println!("Failed to hide welcome window: {}", e);
+        }
+    } else {
+        println!("Welcome window not found");
+    }
 }
